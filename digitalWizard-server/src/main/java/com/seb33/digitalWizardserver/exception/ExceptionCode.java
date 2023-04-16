5f0c2216c2faed5ccd99259a8ba4215b14820ccb
Gonue <6017891@naver.com>
2023-04-16 20:26:33 +0900

    feat : Vote ExceptionCode 추가

--- digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/exception/ExceptionCode.java
+++ digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/exception/ExceptionCode.java
@@ -13,6 +13,7 @@ public enum ExceptionCode {
     QUESTION_NOT_FOUND(404, "질문을 찾을 수 없습니다."),
     ANSWER_NOT_FOUND(404,"답변을 찾을 수 없습니다."),
     INVALID_REQUEST(400, "잘못된 요청 입니다."),
+    ALREADY_VOTE(409,"이미 투표를 하였습니다."),
     INVALID_PERMISSION(403, "권한이 유효하지 않습니다.");
     @Getter
     private int status;
