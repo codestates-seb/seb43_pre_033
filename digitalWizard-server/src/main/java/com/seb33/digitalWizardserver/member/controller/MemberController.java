060cf83d5a5d284475eb243c35f67e473a6a4f80
wish9 <wjwee9@gmail.com>
2023-04-16 21:49:23 +0900

    feat : member 기능구현

--- digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/member/controller/MemberController.java
+++ digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.seb33.digitalWizardserver.member.controller;
 
+import com.seb33.digitalWizardserver.dto.MultiResponseDto;
 import com.seb33.digitalWizardserver.member.dto.MemberDto;
 import com.seb33.digitalWizardserver.member.dto.MemberJoinResponseDto;
 import com.seb33.digitalWizardserver.member.entity.Member;
@@ -7,6 +8,8 @@ import com.seb33.digitalWizardserver.member.mapper.MemberMapper;
 import com.seb33.digitalWizardserver.member.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.net.URI;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -25,40 +29,63 @@ public class MemberController {
     private final MemberMapper memberMapper;
 
     @PostMapping
-    public ResponseEntity<MemberJoinResponseDto> postMember(@Valid @RequestBody MemberDto.PostMember requestBody) {
+    public ResponseEntity postMember(@Valid @RequestBody MemberDto.PostMember requestBody) {
         Member member = memberMapper.memberPostToMember(requestBody);
         Member createMember = memberService.createMember(member);
         MemberJoinResponseDto responseDto = memberMapper.memberToMemberResponse(createMember);
-        return ResponseEntity.ok(responseDto);
+        return new ResponseEntity(responseDto, HttpStatus.CREATED);
     }
-//
-//    @PatchMapping("/{member-id}")
-//    public ResponseEntity patchMember(
+
+    @PatchMapping("/{member-id}") // 케법 케이스 (url 경로에서 주로 사용하는 방식 => 필드값은 카멜케이스로 작성하니까 구분을 위해 사용)
+    public ResponseEntity patchMember(
+            @PathVariable("member-id") @Positive long memberId,
+            @Valid @RequestBody MemberDto.Patch requestBody) {
+        requestBody.setMemberId(memberId);
+
+        Member updateMember = memberService.updateMember(memberMapper.memberPatchToMember(requestBody));
+
+        MemberJoinResponseDto responseDto = memberMapper.memberToMemberResponse(updateMember);
+
+        return new ResponseEntity(responseDto, HttpStatus.OK);
+    }
+
+//    @PatchMapping("/{member-id}/detail")
+//    public ResponseEntity patchMemberDetail(
 //            @PathVariable("member-id") @Positive long memberId,
 //            @Valid @RequestBody MemberDto.Patch requestBody) {
+//        requestBody.setMemberId(memberId);
 //
-//    }
+//        Member updateMember = memberService.updateMember(memberMapper.memberPatchToMember(requestBody));
 //
-//    @GetMapping("/{member-id}")
-//    public ResponseEntity getMember(
-//            @PathVariable("member-id") @Positive long memberId) {
+//        MemberJoinResponseDto responseDto = memberMapper.memberToMemberResponse(updateMember);
 //
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity getMembers(@Positive @RequestParam int page,
-//                                     @Positive @RequestParam int size) {
-//
-//    }
-//
-//    @DeleteMapping("/{member-id}") // 케법 케이스 (url 경로에서 주로 사용하는 방식 => 필드값은 카멜케이스로 작성하니까 구분을 위해 사용)
-//    public ResponseEntity deleteMember(
-//            @PathVariable("member-id") @Positive long memberId) {
-//
+//        return new ResponseEntity(responseDto, HttpStatus.OK);
 //    }
 
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(
+            @PathVariable("member-id") @Positive long memberId) {
+        Member member = memberService.findMember(memberId);
+        MemberJoinResponseDto responseDto = memberMapper.memberToMemberResponse(member);
+        return new ResponseEntity(responseDto, HttpStatus.OK);
+    }
+
     @GetMapping
-    public String getMembers() {
-        return "성공";
+    public ResponseEntity getMembers(@Positive @RequestParam int page,
+                                     @Positive @RequestParam int size) {
+        Page<Member> pageMembers = memberService.findMembers(page - 1, size);
+        List<Member> members = pageMembers.getContent();
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(memberMapper.membersToMemberResponses(members),
+                        pageMembers),
+                HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(
+            @PathVariable("member-id") @Positive long memberId) {
+        memberService.deleteMember(memberId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/member/service/MemberService.java
+++ digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/member/service/MemberService.java
@@ -6,11 +6,15 @@ import com.seb33.digitalWizardserver.exception.ExceptionCode;
 import com.seb33.digitalWizardserver.member.entity.Member;
 import com.seb33.digitalWizardserver.member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +40,51 @@ public class MemberService {
         Member savedMember = memberRepository.save(member);
 
         return savedMember;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public Member updateMember(Member member) {
+        Member findMember = findVerifiedMember(member.getMemberId());
+
+        Optional.ofNullable(member.getMemberNickName())
+                .ifPresent(name -> findMember.setMemberNickName(name));
+        Optional.ofNullable(member.getPassword())
+                .ifPresent(password -> findMember.setEmail(password));
+        Optional.ofNullable(member.getProfileImage())
+                .ifPresent(image -> findMember.setProfileImage(image));
+        Optional.ofNullable(member.getLocation())
+                .ifPresent(location -> findMember.setLocation(location));
+        Optional.ofNullable(member.getTitle())
+                .ifPresent(title -> findMember.setTitle(title));
+        Optional.ofNullable(member.getAboutMe())
+                .ifPresent(aboutMe -> findMember.setAboutMe(aboutMe));
+        return memberRepository.save(findMember);
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMember(long memberId) {
+        return findVerifiedMember(memberId);
+    }
+
+    public Page<Member> findMembers(int page, int size) {
+        return memberRepository.findAll(PageRequest.of(page, size,
+                Sort.by("memberId").descending()));
+    }
+
+    public void deleteMember(long memberId) {
+        Member findMember = findVerifiedMember(memberId);
+
+        memberRepository.delete(findMember);
+    }
+
+    @Transactional(readOnly = true)
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
     }
 
     private void verifyExistsEmail(String email) {
