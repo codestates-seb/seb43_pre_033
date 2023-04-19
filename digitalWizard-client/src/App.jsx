e66271ea5d475b086c65dc0eb2b8094d24515789
hazellee0914 <starmagic2206@gmail.com>
2023-04-19 17:54:28 +0900

    feat : inputBox add

--- digitalWizard-client/src/App.jsx
+++ digitalWizard-client/src/App.jsx
@@ -1,11 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
-=======
-import Header from "./components/Header/Header.jsx";
-import Header from "./components/Header/Header.jsx";
->>>>>>> b61e220f793efb0b258c34e032cf64d22a58cfaa
 // import QuestionDetail from "./pages/QuestionDetail/QuestionDetail.jsx";
 import AskQuestion from "./pages/AskQuestion/AskQuestion.jsx";
 import QuestionSection from "./components/QuestionSection/QuestionSection.jsx";
@@ -21,11 +16,7 @@ function App() {
         <Route path="/question/:questionId" />
         <Route path="/question/:questionId/questionEdit" />
         <Route path="/question/:questionId/answerEdit/:answerId" />
-<<<<<<< HEAD
         <Route path="/users/signup" element={<Signup />} />
-=======
-        <Route path="/users/signup" />
->>>>>>> b61e220f793efb0b258c34e032cf64d22a58cfaa
         <Route path="/users/login" />
       </Routes>
     </div>
