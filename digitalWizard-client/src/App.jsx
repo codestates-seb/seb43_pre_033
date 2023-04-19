7b37a293a3d01ce3f62c0b0388f8d19a919adae4
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-04-19 09:10:05 +0900

    feat : 헤더추가

--- digitalWizard-client/src/App.jsx
+++ digitalWizard-client/src/App.jsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import QuestionDetail from "./pages/QuestionDetail/QuestionDetail.jsx";
+import Header from "./components/Header/Header.jsx";
+// import QuestionDetail from "./pages/QuestionDetail/QuestionDetail.jsx";
+import AskQuestion from "./pages/AskQuestion/AskQuestion.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" />
         <Route path="/question" />
