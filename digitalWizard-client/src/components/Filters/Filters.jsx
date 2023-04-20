18230e62bcade0eeeab1f904d8d8008f972c69cf
ychae1997 <ychae1997@gmail.com>
2023-04-20 16:20:34 +0900

    feat : question title, filter compo

--- digitalWizard-client/src/components/Filters/Filters.jsx
+++ digitalWizard-client/src/components/Filters/Filters.jsx
@@ -1,51 +1,108 @@
+import { useState } from "react";
 import styles from "./Filters.module.css";
-import { MdArrowDropDown } from "react-icons/md";
+import { MdArrowDropDown, MdFilterList } from "react-icons/md";
 
-export default function Filters() {
-  const size = 23652257;
-  const filterList = [
-    {
-      legend: "Filter by",
-      type: "checkbox",
-      item: ["No answers", "No accepted answer", "Has bounty"],
-    },
-    {
-      legend: "Sorted by",
-      type: "radio",
-      item: [
-        "Newest",
-        "Recent activity",
-        "Highest score",
-        "Most frequent",
-        "Bounty ending soon",
-      ],
-    },
-  ];
+export default function Filters({ size }) {
+  const sorted = ["Newest", "Active", "Bountied", "Unanswered"];
+  const [selected, setSelected] = useState("Newest");
+  const [filter, setFilter] = useState(false);
+
   return (
     <>
       <div className={styles.filters}>
-        <span className={styles.size}>{size.toLocaleString()} questions</span>
-        <div className={styles.sortedWrap}>
-          <button className={styles.sorted}>Newest</button>
-          <button className={styles.sorted}>Active</button>
-          <button className={styles.sorted}>
-            Bountied <span className={styles.tag}>230</span>
-          </button>
-          <button className={styles.sorted}>Unanswered</button>
-          <button className={styles.sorted}>
-            More <MdArrowDropDown />
+        <span className={styles.size}>{size} questions</span>
+        <div className={styles.btnGroup}>
+          <div className={styles.sortedWrap}>
+            {sorted.map((btn, i) => (
+              <button
+                key={i}
+                className={`${styles.sorted} ${
+                  selected === btn && styles.selected
+                } `}
+                onClick={() => setSelected(btn)}>
+                {btn}
+                {btn === "Bountied" && <span className={styles.tag}>230</span>}
+              </button>
+            ))}
+            <button className={styles.sorted}>
+              More <MdArrowDropDown />
+            </button>
+          </div>
+          <button
+            className={`btnSub ${styles.btnFilter} ${
+              filter && styles.selected
+            }`}
+            onClick={() => setFilter(prev => !prev)}>
+            <MdFilterList />
+            Filter
           </button>
         </div>
-        <button className={styles.btnFilter}></button>
       </div>
-      <div className={styles.dropFilter}>
-        <fieldset>
-          <legend>Filter by</legend>
-          <ul>
-            <li></li>
-          </ul>
-        </fieldset>
-      </div>
+
+      {filter && (
+        <form className={styles.dropFilter}>
+          <div className={styles.filterWrap}>
+            {filterList.map(filter => (
+              <fieldset className={styles.fieldset} key={filter.legend}>
+                <legend className={styles.legend}>{filter.legend}</legend>
+                <ul>
+                  {filter.item.map((list, i) => (
+                    <li className={styles.list} key={filter.legend + i}>
+                      <input
+                        id={list}
+                        type={filter.type}
+                        name={filter}
+                        value={list}
+                      />
+                      <label htmlFor={list}>{list}</label>
+                    </li>
+                  ))}
+                  {filter.legend === "Tagged with" && (
+                    <input
+                      className={styles.inputTag}
+                      type="text"
+                      placeholder="e.g. javascript or python"
+                    />
+                  )}
+                </ul>
+              </fieldset>
+            ))}
+          </div>
+          <div className={styles.btnWrap}>
+            <button className="btn btnPrimary">Apply filter</button>
+            <button className="btn btnSub">Save custom filter</button>
+            <button
+              onClick={() => setFilter(prev => !prev)}
+              className={styles.btnCancle}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      )}
     </>
   );
 }
+
+export const filterList = [
+  {
+    legend: "Filter by",
+    type: "checkbox",
+    item: ["No answers", "No accepted answer", "Has bounty"],
+  },
+  {
+    legend: "Sorted by",
+    type: "radio",
+    item: [
+      "Newest",
+      "Recent activity",
+      "Highest score",
+      "Most frequent",
+      "Bounty ending soon",
+    ],
+  },
+  {
+    legend: "Tagged with",
+    type: "radio",
+    item: ["My watched tags", "The following tags:"],
+  },
+];
--- digitalWizard-client/src/components/Headline/Headline.jsx
+++ digitalWizard-client/src/components/Headline/Headline.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Headline.module.css";
 
 export default function Headline({ h2 }) {
+  const navigate = useNavigate();
   return (
     <div className={styles.headline}>
       <h2 className={styles.title}>{h2}</h2>
-      <button className="btn btnPrimary">Ask Question</button>
+      <button
+        onClick={() => navigate("/question/ask")}
+        className={"btn btnPrimary"}>
+        Ask Question
+      </button>
     </div>
   );
 }
