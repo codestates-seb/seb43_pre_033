937caf1b0983da34c0cbb899c701cb48e35dadf6
ychae1997 <ychae1997@gmail.com>
2023-04-21 05:24:04 +0900

    feat : 질문목록, 질문상세 페이지 라우팅

--- digitalWizard-client/src/App.jsx
+++ digitalWizard-client/src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
-// import QuestionDetail from "./pages/QuestionDetail/QuestionDetail.jsx";
+import QuestionDetail from "./pages/QuestionDetail/QuestionDetail.jsx";
 import AskQuestion from "./pages/AskQuestion/AskQuestion.jsx";
-import QuestionSection from "./components/QuestionSection/QuestionSection.jsx";
+import QuestionSection from "./pages/QuestionSection/QuestionSection.jsx";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" />
         <Route path="/question" element={<QuestionSection />} />
         <Route path="/question/ask" element={<AskQuestion />} />
-        <Route path="/question/:questionId" />
+        <Route path="/question/:questionId" element={<QuestionDetail />} />
         <Route path="/question/:questionId/questionEdit" />
         <Route path="/question/:questionId/answerEdit/:answerId" />
         <Route path="/users/signup" element={<Signup />} />
