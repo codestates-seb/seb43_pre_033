c23ec7d612c9975c5d46eaf010c636e0c39318c9
ychae1997 <ychae1997@gmail.com>
2023-04-21 05:20:35 +0900

    feat : 질문목록 빈페이지 추가, 반응형

--- digitalWizard-client/src/components/QuestionSection/QuestionSection.jsx
+++ digitalWizard-client/src/components/QuestionSection/QuestionSection.jsx
@@ -3,26 +3,39 @@ import Headline from "../Headline/Headline.jsx";
 import styles from "./QuestionSection.module.css";
 import QuestionList from "../QeustionList/QuestionList.jsx";
 import { useEffect, useState } from "react";
-import { getQuestion } from "../../api/questionApi";
+import { getQuestion } from "../../api/questionApi.js";
+import { RiFolderWarningLine } from "react-icons/ri";
 
 export default function QuestionSection() {
   const [questions, setQuestions] = useState(null);
 
   useEffect(() => {
-    getQuestion("/question").then(data => setQuestions(data));
+    getQuestion("/question").then(data => {
+      // getQuestion("/question?size=20&page=0").then(data => {
+      // setQuestions(data.result.content);
+      setQuestions(data);
+      console.log(data);
+    });
   }, []);
 
   return (
     <section className={styles.section}>
       <div className={styles.mainbar}>
         <div className={styles.titleWrap}>
-          <Headline h2={"All Questions"} />
-          <Filters size={questions && questions.length.toLocaleString()} />
+          <Headline h2={"Top Questions"} />
+          <Filters />
         </div>
-        <div className={styles.questionsWrap}>
-          <QuestionList questions={questions} />
-          {/* pager */}
-        </div>
+        {questions ? (
+          <div className={styles.questionsWrap}>
+            <QuestionList questions={questions} />
+            {/* pager */}
+          </div>
+        ) : (
+          <div className={styles.empty}>
+            <RiFolderWarningLine className={styles.emptyIco} />
+            <p className={styles.emptyMent}>질문 목록이 없습니다.</p>
+          </div>
+        )}
       </div>
       {/* 오른쪽 사이드바 aside compo */}
     </section>
