fbd6e7de5612a491eb721298d28c4a085c9dc40c
ychae1997 <ychae1997@gmail.com>
2023-04-21 05:00:12 +0900

    refactor : flexible req

--- digitalWizard-client/src/api/questionApi.js
+++ digitalWizard-client/src/api/questionApi.js
@@ -1,18 +1,20 @@
 import request from "./core";
 
 // get
-const getQuestion = () => {
-  return request({ url: "/question" });
+const getQuestion = url => {
+  return request({ url });
 };
 
 // post
-const postQuestion = data => {
+const postQuestion = (data, url) => {
   return request({
     method: "post",
-    url: "/question",
+    url,
     data,
     headers: {
       "Content-Type": "application/json",
+      // Authorization:
+      //   "WishJWT eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJVU0VSIl0sIm1lbWJlckVtYWlsIjoidGVzdDFAZ21haWwuY29tIiwic3ViIjoidGVzdDFAZ21haWwuY29tIiwiaWF0IjoxNjgyMDA2NTg2LCJleHAiOjE2ODIwMDgzODZ9.P1TzkkxHjcytIPQsINB_P68JQdFmY-IfFGy3FRmoQhI",
     },
   });
 };
