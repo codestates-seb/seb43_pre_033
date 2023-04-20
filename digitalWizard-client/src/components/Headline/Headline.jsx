15154fab586ea8b6aa8ac527e7f3335399ef901e
ychae1997 <ychae1997@gmail.com>
2023-04-21 04:54:26 +0900

    design : headline 반응형

--- digitalWizard-client/src/components/Headline/Headline.jsx
+++ digitalWizard-client/src/components/Headline/Headline.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Headline.module.css";
 
-export default function Headline({ h2 }) {
+export default function Headline({ h2, position }) {
   const navigate = useNavigate();
   return (
-    <div className={styles.headline}>
+    <div className={`${position && styles.detail} ${styles.headline}`}>
       <h2 className={styles.title}>{h2}</h2>
       <button
         onClick={() => navigate("/question/ask")}
