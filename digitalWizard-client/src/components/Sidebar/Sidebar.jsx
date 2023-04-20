8f8e2834f637682053e7315b3eb38a94c90ac50f
SEB_FE_43_차재현 <119926151+CHA710617@users.noreply.github.com>
2023-04-20 17:38:09 +0900

    feat : sidebar component

---
+++ digitalWizard-client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      
+    </div>
+  )
+}
+
+export default Sidebar
