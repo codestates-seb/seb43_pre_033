2452eadfce35e274ad000761ca7c42fc9b6acaaa
ychae1997 <ychae1997@gmail.com>
2023-04-21 15:43:27 +0900

    feat : comment, edit 연결

--- digitalWizard-client/src/components/Post/Post.jsx
+++ digitalWizard-client/src/components/Post/Post.jsx
@@ -5,7 +5,8 @@ import { ImCheckmark } from "react-icons/im";
 import { RxCounterClockwiseClock } from "react-icons/rx";
 import { useState } from "react";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import Comment from "../Comment/Comment.jsx";
 
 //
 function Post({ data, QA }) {
@@ -82,55 +83,86 @@ function Post({ data, QA }) {
     });
   }
 
-  return (
-    <div className={styles.post}>
-      <div className={styles.vote}>
-        <AiFillCaretUp className={styles.up} />
-        <div>{result}</div>
-        <AiFillCaretDown className={styles.down} />
-        {bookmark ? (
-          <FaBookmark className={styles.checkMark} />
-        ) : (
-          <FaRegBookmark className={styles.mark} />
-        )}
+  // Edit Routing
+  const navigate = useNavigate();
+  const handleEdit = () => {
+    QA === "Q" &&
+      navigate(`/question/${data.id}/questionEdit`, { state: data });
+    QA === "A" && navigate(`/question/:questionId/answerEdit/:answerId`);
+    // 영은님 나중에 서버 연결되면 경로에 맞게 수정하세요 :)
+  };
 
-        {QA === "A" && data.accepted ? (
-          <ImCheckmark className={styles.checkin} />
-        ) : null}
-        <RxCounterClockwiseClock className={styles.clock} />
-      </div>
-      <div className={styles.detail}>
-        <div className={styles.content}>{data.body}</div>
-        <div className={styles.bottom}>
-          <div className={styles.share}>
-            <div>Share</div>
-            <div>Edit</div>
-            <div>Follow</div>
-          </div>
-          <div className={styles.edtied}>edited {edited}</div>
-          <div className={styles.profile}>
-            <div className={styles.create}>
-              {QA === "A" ? "answered " + create : "aked " + create}
-            </div>
-            <div className={styles.user}>
-              <img
-                src={data.member.profileImage}
-                alt="아바타이미지"
-                className={styles.img}
+  return (
+    <>
+      <div className={`${QA === "A" && styles.border} ${styles.postWrap}`}>
+        <div className={styles.post}>
+          <div className={styles.vote}>
+            <AiFillCaretUp className={styles.up} />
+            <div>{result}</div>
+            <AiFillCaretDown className={styles.down} />
+            {bookmark ? (
+              <FaBookmark
+                onClick={() => setBookmark(prev => !prev)}
+                className={styles.checkMark}
               />
-              <div className={styles.userDetail}>
-                <div className={styles.name}>{data.member.memberNickName}</div>
-                <div>
-                  <span className={styles.userScore}>1,463</span>
-                  <span className={styles.silver}>8</span>
-                  <span className={styles.bronze}>8</span>
+            ) : (
+              <FaRegBookmark
+                onClick={() => setBookmark(prev => !prev)}
+                className={styles.mark}
+              />
+              // bookmark click event 추가
+            )}
+
+            {QA === "A" && data.accepted ? (
+              <ImCheckmark className={styles.checkin} />
+            ) : null}
+            <RxCounterClockwiseClock className={styles.clock} />
+          </div>
+          <div className={styles.detail}>
+            <div className={styles.content}>{data.body}</div>
+            <div className={styles.bottom}>
+              <div className={styles.share}>
+                <div>Share</div>
+                <div onClick={handleEdit} role="none">
+                  Edit
+                </div>
+                <div>Follow</div>
+              </div>
+              {QA === "A" && create !== edited && (
+                <div className={styles.edtied}>edited {edited}</div>
+                // 답변이 수정 되었을 때만 렌더링
+              )}
+              <div
+                className={`${QA === "Q" && styles.hasBg} ${styles.profile}`}>
+                {/* 작성자 프로필 배경색 추가 class */}
+                <div className={styles.create}>
+                  {QA === "A" ? "answered " + create : "aked " + create}
+                </div>
+                <div className={styles.user}>
+                  <img
+                    src={data.member.profileImage}
+                    alt="아바타이미지"
+                    className={styles.img}
+                  />
+                  <div className={styles.userDetail}>
+                    <div className={styles.name}>
+                      {data.member.memberNickName}
+                    </div>
+                    <div>
+                      <span className={styles.userScore}>1,463</span>
+                      <span className={styles.silver}>8</span>
+                      <span className={styles.bronze}>8</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <Comment id={data.id} />
+        {/* 댓글 컴포넌트 추가, 답변만 border 추가 */}
       </div>
-    </div>
+    </>
   );
 }
 
