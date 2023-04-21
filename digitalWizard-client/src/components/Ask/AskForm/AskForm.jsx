d296bf50af15369d1d4355d7349c774dbfcb1ada
ychae1997 <ychae1997@gmail.com>
2023-04-21 16:57:08 +0900

    feat : flexible form

--- digitalWizard-client/src/components/Ask/AskForm/AskForm.jsx
+++ digitalWizard-client/src/components/Ask/AskForm/AskForm.jsx
@@ -5,20 +5,39 @@ import Dialog from "../Dialog/Dialog.jsx";
 import useModal from "../../../hooks/useMdoal";
 import useInput from "../../../hooks/useInput";
 import { postQuestion } from "../../../api/questionApi";
+import { useNavigate } from "react-router-dom";
 
 export default function AskForm() {
   const [modal, openModal, closeModal] = useModal(false);
   const [titleValue, titleReset] = useInput("");
   const [tagValue, tagReset] = useInput("");
   const [bodyValue, bodyReset] = useInput("", true);
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
+    // test용 payload
     const payload = {
       title: titleValue.value,
       body: bodyValue.value,
+      vote: 0,
+      view: 0,
+      answerCount: 0,
+      createdAt: new Date(),
+      modifiedAt: "2023-04-17T14:42:40.279535",
+      member: {
+        memberId: 1,
+        email: "cat@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1574158622682-e40e69881006?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+        memberNickName: "김야옹",
+      },
     };
-    postQuestion(payload);
-    // redirect home 추가해야함!!
+    // const payload = {
+    //   title: titleValue.value,
+    //   body: bodyValue.value,
+    // };
+    postQuestion(payload, "/question");
+    navigate("/question");
   };
 
   return (
@@ -30,7 +49,12 @@ export default function AskForm() {
           para="Be specific and imagine you’re asking a question to another person."
           placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
         />
-        <TextEditor value={bodyValue} />
+        <TextEditor
+          type="What are the details of your problem?"
+          value={bodyValue}
+          para="Introduce the problem and expand on what you put in the title. Minimum
+          20 characters."
+        />
         <Input
           type="tags"
           value={tagValue}
--- digitalWizard-client/src/components/Ask/Form/Input.jsx
+++ digitalWizard-client/src/components/Ask/Form/Input.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import AskTip from "../AskTip/AskTip.jsx";
 import styles from "./Form.module.css";
 
-export default function Input({ type, value, para, placeholder }) {
+export default function Input({
+  type,
+  value,
+  para,
+  placeholder,
+  edit = false,
+}) {
   const [focus, setFocus] = useState(false);
 
   return (
     <div className={styles.inputWrap}>
-      {focus && <AskTip type={type} />}
-      <div className={styles.inputBox}>
+      {focus && <AskTip type={type} edit={edit} />}
+      <div className={`${edit && styles.editVer} ${styles.inputBox}`}>
         <label className={styles.title} htmlFor={type}>
           {type}
         </label>
