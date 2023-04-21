4749ee77c928c3f5e555c26058deb96f354d1f3b
ychae1997 <ychae1997@gmail.com>
2023-04-21 19:11:46 +0900

    feat : filter 수정

--- digitalWizard-client/src/components/Filters/Filters.jsx
+++ digitalWizard-client/src/components/Filters/Filters.jsx
@@ -1,51 +1,25 @@
+import { useState } from "react";
 import styles from "./Filters.module.css";
-import { MdArrowDropDown } from "react-icons/md";
 
 export default function Filters() {
-  const size = 23652257;
-  const filterList = [
-    {
-      legend: "Filter by",
-      type: "checkbox",
-      item: ["No answers", "No accepted answer", "Has bounty"],
-    },
-    {
-      legend: "Sorted by",
-      type: "radio",
-      item: [
-        "Newest",
-        "Recent activity",
-        "Highest score",
-        "Most frequent",
-        "Bounty ending soon",
-      ],
-    },
-  ];
+  const sorted = ["Interestion", "Bountied", "Hot", "Week", "Month"];
+  const [selected, setSelected] = useState("Interestion");
+
   return (
-    <>
-      <div className={styles.filters}>
-        <span className={styles.size}>{size.toLocaleString()} questions</span>
-        <div className={styles.sortedWrap}>
-          <button className={styles.sorted}>Newest</button>
-          <button className={styles.sorted}>Active</button>
-          <button className={styles.sorted}>
-            Bountied <span className={styles.tag}>230</span>
+    <div className={styles.filters}>
+      <div className={styles.sortedWrap}>
+        {sorted.map((btn, i) => (
+          <button
+            key={i}
+            className={`${styles.sorted} ${
+              selected === btn && styles.selected
+            } `}
+            onClick={() => setSelected(btn)}>
+            {btn}
+            {btn === "Bountied" && <span className={styles.tag}>230</span>}
           </button>
-          <button className={styles.sorted}>Unanswered</button>
-          <button className={styles.sorted}>
-            More <MdArrowDropDown />
-          </button>
-        </div>
-        <button className={styles.btnFilter}></button>
+        ))}
       </div>
-      <div className={styles.dropFilter}>
-        <fieldset>
-          <legend>Filter by</legend>
-          <ul>
-            <li></li>
-          </ul>
-        </fieldset>
-      </div>
-    </>
+    </div>
   );
 }
