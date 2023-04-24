74dc02679b3cae58766a675c3b28173f5e179d77
ychae1997 <ychae1997@gmail.com>
2023-04-25 06:19:15 +0900

    feat : flexible filter

--- digitalWizard-client/src/components/Filters/Filters.jsx
+++ digitalWizard-client/src/components/Filters/Filters.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
 import styles from "./Filters.module.css";
 
-export default function Filters() {
-  const sorted = ["Interestion", "Bountied", "Hot", "Week", "Month"];
-  const [selected, setSelected] = useState("Interestion");
+export default function Filters({ sorted }) {
+  const [selected, setSelected] = useState(sorted[0]);
 
   return (
     <div className={styles.filters}>
       <div className={styles.sortedWrap}>
-        {sorted.map((btn, i) => (
-          <button
-            key={i}
-            className={`${styles.sorted} ${
-              selected === btn && styles.selected
-            } `}
-            onClick={() => setSelected(btn)}>
-            {btn}
-            {btn === "Bountied" && <span className={styles.tag}>230</span>}
-          </button>
-        ))}
+        {sorted &&
+          sorted.map((btn, i) => (
+            <button
+              key={i}
+              className={`${styles.sorted} ${
+                selected === btn && styles.selected
+              } `}
+              onClick={() => setSelected(btn)}>
+              {btn}
+              {btn === "Bountied" && <span className={styles.tag}>230</span>}
+            </button>
+          ))}
       </div>
     </div>
   );
