c839ce05752504c34d745bfcf037d0b253285f19
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-04-24 16:18:47 +0900

    design : 네비 헤더에서 분리

--- digitalWizard-client/src/components/Header/Header.jsx
+++ digitalWizard-client/src/components/Header/Header.jsx
@@ -3,85 +3,125 @@ import { HiOutlineSearch } from "react-icons/hi";
 import { BsFillInboxFill } from "react-icons/bs";
 import { AiTwotoneTrophy, AiFillQuestionCircle } from "react-icons/ai";
 import { FaStackExchange } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../images/logo.svg";
+import SidebarL from "../Sidebar/SidebarL/SidebarL.jsx";
 import { useIsLoginStore } from "../../stores/loginStore";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState, useEffect } from "react";
 
 const Header = ({ onSearch, search }) => {
   const { isLogin } = useIsLoginStore(state => state);
   const dummyImg =
     "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
 
+  const [modal, setModal] = useState(false); // 메뉴 열고닫기
+  const [hide, setHide] = useState(false); // url에 따른 메뉴 숨김
+  const location = useLocation();
+  const path = location.pathname;
+  useEffect(() => {
+    switch (path) {
+      case "/users/login":
+        setHide(true);
+        break;
+      case "/users/signup":
+        setHide(true);
+        break;
+      case "/question/ask":
+        setHide(true);
+        break;
+      default:
+        setHide(false);
+    }
+    setModal(false);
+  }, [path]);
+
   return (
-    <header className={styles.header}>
-      <div className={styles.wrap}>
-        <RxHamburgerMenu className={styles.menuIcon} />
-        <div className={styles.logoWrap}>
-          <Link to="/" className={styles.logo}>
-            <img src={logo} alt="logo" className={styles.logoImg}></img>
-          </Link>
-          <ul className={`${styles.menuList} ${isLogin && styles.login}`}>
-            <li className={styles.menu}>
-              <Link className={styles.none} to="#">
-                About
-              </Link>
-            </li>
-            <li className={styles.menu}>
-              <Link className={styles.link} to="/question">
-                Products
-              </Link>
-            </li>
-            <li className={styles.menu}>
-              <Link className={styles.none} to="#">
-                For Teams
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <form className={styles.searchBar}>
-          <HiOutlineSearch className={styles.searchIco} />
-          <input
-            {...search}
-            className={styles.search}
-            placeholder="Search..."
-            onKeyDown={e => onSearch(e)}
+    <>
+      <header className={styles.header}>
+        <div className={styles.wrap}>
+          <RxHamburgerMenu
+            className={hide ? styles.menuIcon : styles.menuIconBlock}
+            onClick={() => setModal(!modal)}
           />
-        </form>
-        {isLogin ? (
-          <div className={styles.loginBox}>
-            <button className={styles.profile}>
-              <img className={styles.img} src={dummyImg} alt="dummy img" />
-            </button>
-            <button className={styles.icon}>
-              <BsFillInboxFill />
-            </button>
-            <button className={styles.icon}>
-              <AiTwotoneTrophy />
-            </button>
-            <button className={styles.icon}>
-              <AiFillQuestionCircle />
-            </button>
-            <button className={styles.icon}>
-              <FaStackExchange />
-            </button>
+          <div className={styles.logoWrap}>
+            <Link to="/" className={styles.logo}>
+              <img src={logo} alt="logo" className={styles.logoImg}></img>
+            </Link>
+            <ul className={`${styles.menuList} ${isLogin && styles.login}`}>
+              <li className={styles.menu}>
+                <Link className={styles.none} to="#">
+                  About
+                </Link>
+              </li>
+              <li className={styles.menu}>
+                <Link className={styles.link} to="/question">
+                  Products
+                </Link>
+              </li>
+              <li className={styles.menu}>
+                <Link className={styles.none} to="#">
+                  For Teams
+                </Link>
+              </li>
+            </ul>
           </div>
-        ) : (
-          <ul className={styles.menubar}>
-            <li className={styles.list}>
-              <Link className="btnSub" to="/users/login">
-                Log in
-              </Link>
-            </li>
-            <li className={styles.list}>
-              <Link className="btnPrimary" to="/users/signup">
-                Sign up
-              </Link>
-            </li>
-          </ul>
-        )}
-      </div>
-    </header>
+          <form className={styles.searchBar}>
+            <HiOutlineSearch className={styles.searchIco} />
+            <input
+              {...search}
+              className={styles.search}
+              placeholder="Search..."
+              onKeyDown={e => onSearch(e)}
+            />
+          </form>
+          {isLogin ? (
+            <div className={styles.loginBox}>
+              <button className={styles.profile}>
+                <img className={styles.img} src={dummyImg} alt="dummy img" />
+              </button>
+              <button className={styles.icon}>
+                <BsFillInboxFill />
+              </button>
+              <button className={styles.icon}>
+                <AiTwotoneTrophy />
+              </button>
+              <button className={styles.icon}>
+                <AiFillQuestionCircle />
+              </button>
+              <button className={styles.icon}>
+                <FaStackExchange />
+              </button>
+            </div>
+          ) : (
+            <ul className={styles.menubar}>
+              <li className={styles.list}>
+                <Link className="btnSub" to="/users/login">
+                  Log in
+                </Link>
+              </li>
+              <li className={styles.list}>
+                <Link className="btnPrimary" to="/users/signup">
+                  Sign up
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </header>
+      <nav
+        className={
+          hide
+            ? modal
+              ? styles.sidebarHide
+              : styles.sidebarHideNone
+            : modal
+            ? styles.sidebar
+            : styles.sidebarNone
+        }>
+        <SidebarL modal={modal} setModal={setModal} />
+      </nav>
+    </>
   );
 };
 
