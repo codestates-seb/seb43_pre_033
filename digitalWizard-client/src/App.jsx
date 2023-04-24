55580b753fa92eddfb760dd0033e79a415360d94
ychae1997 <ychae1997@gmail.com>
2023-04-24 14:23:07 +0900

    feat : 검색 keypress

--- digitalWizard-client/src/App.jsx
+++ digitalWizard-client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
 import QuestionDetail from "./pages/QuestionDetail/QuestionDetail.jsx";
@@ -6,31 +6,37 @@ import AskQuestion from "./pages/AskQuestion/AskQuestion.jsx";
 import QuestionSection from "./pages/QuestionSection/QuestionSection.jsx";
 import Login from "./pages/Login/Login.jsx";
 import AskQuestionEdit from "./pages/AskQuestionEdit/AskQuestionEdit.jsx";
-import SidebarL from "./components/Sidebar/SidebarL/SidebarL.jsx";
+import SearchPage from "./pages/QuestionSection/SearchPage.jsx";
+import useInput from "./hooks/useInput.js";
 
 function App() {
-  const path = /^\/users.*$|^\/question\/ask$/;
-  const showSidebar = !path.test(useLocation().pathname);
+  const [search, searchReset] = useInput("");
+  const navigate = useNavigate();
+
+  const handleSearch = e => {
+    if (e.key === "Enter") {
+      navigate(`/question/search?keyworkd=${search.value}`);
+      // getQuestion(`/question/search?keyword=${search.value}&page=0&size=10`);
+    }
+  };
 
   return (
     <div className="App">
-      <Header />
-      <div className={`${showSidebar && "container"}`}>
-        {showSidebar && <SidebarL />}
-        <Routes>
-          <Route path="/" />
-          <Route path="/question" element={<QuestionSection />} />
-          <Route path="/question/ask" element={<AskQuestion />} />
-          <Route path="/question/:questionId" element={<QuestionDetail />} />
-          <Route
-            path="/question/:questionId/questionEdit"
-            element={<AskQuestionEdit />}
-          />
-          <Route path="/question/:questionId/answerEdit/:answerId" />
-          <Route path="/users/signup" element={<Signup />} />
-          <Route path="/users/login" element={<Login />} />
-        </Routes>
-      </div>
+      <Header onSearch={handleSearch} search={search} />
+      <Routes>
+        <Route path="/" />
+        <Route path="/question" element={<QuestionSection />} />
+        <Route path="/question/search" element={<SearchPage />} />
+        <Route path="/question/ask" element={<AskQuestion />} />
+        <Route path="/question/:questionId" element={<QuestionDetail />} />
+        <Route
+          path="/question/:questionId/questionEdit"
+          element={<AskQuestionEdit />}
+        />
+        <Route path="/question/:questionId/answerEdit/:answerId" />
+        <Route path="/users/signup" element={<Signup />} />
+        <Route path="/users/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
