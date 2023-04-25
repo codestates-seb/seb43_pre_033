c88b552b817ef7cea850cc1c447ab5ff16c4e92d
ychae1997 <ychae1997@gmail.com>
2023-04-25 16:10:56 +0900

    style : 필요없는 코드 삭제

--- digitalWizard-client/src/pages/QuestionSection/SearchPage.jsx
+++ digitalWizard-client/src/pages/QuestionSection/SearchPage.jsx
@@ -1,11 +1,12 @@
 import styles from "./QuestionSection.module.css";
 import Headline from "../../components/Headline/Headline.jsx";
 import Filters from "../../components/Filters/Filters.jsx";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getQuestion } from "../../api/questionApi";
 import { RiFolderWarningLine } from "react-icons/ri";
 import QuestionList from "../../components/QeustionList/QuestionList.jsx";
+import SidebarR from "../../components/Sidebar/SidebarR/SidebarR.jsx";
 import Pagination from "../../common/Pagination/Pagination.jsx";
 
 export default function SearchPage({ pageNum, setPageNum }) {
@@ -18,13 +19,9 @@ export default function SearchPage({ pageNum, setPageNum }) {
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    setPageNum(0); // keyword가 변경될 때마다 pageNum을 0으로 초기화
-  }, [keyword]);
-
-  useEffect(() => {
     if (keyword.trim() !== "") {
       getQuestion(
-        `/question/search?keyword=${keyword}&page=${pageNum}&size=2`
+        `/question/search?keyword=${keyword}&page=${pageNum}&size=10`
       ).then(data => {
         setisEmpty(data.result.empty);
         setQuestions(data.result.content);
@@ -40,7 +37,7 @@ export default function SearchPage({ pageNum, setPageNum }) {
       <div className={styles.mainbar}>
         <div className={styles.titleWrap}>
           <Headline h2={"Search Results"} />
-          <ul>
+          <ul className={styles.searchInfo}>
             <li>
               Results for <strong>{keyword}</strong>
             </li>
@@ -69,6 +66,7 @@ export default function SearchPage({ pageNum, setPageNum }) {
           </div>
         )}
       </div>
+      <SidebarR />
     </section>
   );
 }
