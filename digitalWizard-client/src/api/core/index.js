fb586436eb17806eb3b1c1c9c18fcdc7252bf17c
ychae1997 <ychae1997@gmail.com>
2023-04-26 07:19:28 +0900

    fix : token 재발급 에러 수정

--- digitalWizard-client/src/api/core/index.js
+++ digitalWizard-client/src/api/core/index.js
@@ -1,26 +1,22 @@
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 // create an axios instance
 const request = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 2500,
 });
-
-request.defaults.timeout = 2500;
 
 // req interceptors
 request.interceptors.request.use(
   config => {
     const token = localStorage.getItem("accessToken");
-    // if (token) {
-    //   config.headers.authorization = token;
-    // }
-
-    const refreshToken = localStorage.getItem("refreshToken");
-    if (config.url === "/refresh") {
-      config.headers.authorization = refreshToken;
-    }
-
-    if (token && config.url !== "/members/login" && config.url !== "/refresh") {
+    if (
+      token &&
+      config.url !== "/members/login" &&
+      config.url !== "/members/signup" &&
+      config.url !== "/refresh"
+    ) {
       config.headers.authorization = token;
     }
 
@@ -34,43 +30,58 @@ request.interceptors.request.use(
 
 // res interceptors
 request.interceptors.response.use(
-  response =>
-    response.config.url === "/members/login" ? response.headers : response.data,
+  response => {
+    return response.config.url === "/members/login" ||
+      response.config.url === "/members/signup" ||
+      response.config.url === "/refresh"
+      ? response.headers
+      : response.data;
+  },
   error => {
-    // console.log(error);
+    console.log(error);
     const originalRequest = error.config;
-    // originalRequest._retry = true;
-    console.log(originalRequest._retry);
-    // if (error.response.status === 401) {
-    //   // 백엔드에서 액세스 토큰을 안준다,,?
-    //   // get new token from refresh token
-    // ! 로컬스토리지 삭제하기
-    //   const refreshToken = localStorage.getItem("refreshToken");
-    //   return request({
-    //     method: "post",
-    //     url: "/refresh",
-    //     // refresh 토큰이 있는데 일치하지 않을때
-    //     // 탈출조건
-    //   })
-    //     .then(res => {
-    //       // save new token to localStorage
-    //       const accessToken = res.headers["authorization"]; // 토큰 정보 추출
-    //       const refreshToken = res.headers["refresh"]; // 토큰 정보 추출
-    //       accessToken && localStorage.setItem("accessToken", accessToken);
-    //       refreshToken && localStorage.setItem("refreshToken", refreshToken);
-    //       // update Authorization header and retry original request
-    //       originalRequest.headers.authorization = accessToken;
-    //       return request(originalRequest);
-    //     })
-    //     .catch(error => {
-    //       console.log(error); // debug
-    //       return Promise.reject(error);
-    //     });
-    // } else {
-    //   console.log(error); // debug
-    //   return Promise.reject(error);
-    // }
+
+    if (error.response.status === 401 && isExpired()) {
+      // get new token from refresh token
+      const refreshToken = localStorage.getItem("refreshToken");
+      return request({
+        method: "post",
+        url: "/refresh",
+        headers: {
+          "Content-Type": "application/json",
+          withCredentials: true,
+          refresh: refreshToken,
+        },
+      })
+        .then(res => {
+          const { authorization } = res;
+
+          // save new token to localStorage
+          console.log(authorization);
+          authorization && localStorage.setItem("accessToken", authorization);
+
+          // update Authorization header and retry original request
+          originalRequest.headers.authorization = authorization;
+          originalRequest.retry = true; // set retry flag
+          return request(originalRequest); // retry
+        })
+        .catch(error => {
+          console.log(error); // debug
+          return Promise.reject(error);
+        });
+    } else {
+      console.log(error); // debug
+      console.log(error.response.data); // debug
+      return Promise.reject(error);
+    }
   }
 );
+
+function isExpired() {
+  const token = localStorage.getItem("accessToken");
+  const currentTime = Date.now() / 1000;
+  const decodeToken = jwt_decode(token);
+  return decodeToken.exp < currentTime ? true : false;
+}
 
 export default request;
