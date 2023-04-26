936f01b49e6b26ad04e6d2ab53ed0baa51804a86
wish9 <wjwee9@gmail.com>
2023-04-26 15:42:06 +0900

    fix : 재발급한 엑세스토큰 오류 수정

--- digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/auth/refresh/RefreshController.java
+++ digitalWizard-server/src/main/java/com/seb33/digitalWizardserver/auth/refresh/RefreshController.java
@@ -1,6 +1,8 @@
 package com.seb33.digitalWizardserver.auth.refresh;
 
 import com.seb33.digitalWizardserver.auth.jwt.JwtTokenizer;
+import com.seb33.digitalWizardserver.member.entity.Member;
+import com.seb33.digitalWizardserver.member.repository.MemberRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
@@ -13,13 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/refresh")
 @AllArgsConstructor
 public class RefreshController {
     private final JwtTokenizer jwtTokenizer;
-
+    private final MemberRepository memberRepository;
     @PostMapping
     public ResponseEntity<String> refreshAccessToken(HttpServletRequest request) { // 리프레쉬 토큰 받으면 엑세스 토큰 재발급
         String refreshToken = request.getHeader("Refresh");
@@ -29,16 +34,37 @@ public class RefreshController {
                 Jws<Claims> claims = jwtTokenizer.getClaims(refreshToken, jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey()));
 
                 String email = claims.getBody().getSubject();
-                Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
-                String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+                Optional<Member> optionalMember = memberRepository.findByEmail(email);
 
-                String accessToken = jwtTokenizer.generateAccessToken(claims.getBody(), email, expiration, base64EncodedSecretKey);
-                return ResponseEntity.ok().header("Authorization", "WishJWT " + accessToken).body("Access token refreshed");
+                if (optionalMember.isPresent()) {
+                    Member member = optionalMember.get();
+                    String accessToken = delegateAccessToken(member);
+
+                    return ResponseEntity.ok().header("Authorization", "WishJWT " + accessToken).body("Access token refreshed");
+                } else {
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid member email");
+                }
             } catch (JwtException e) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
             }
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing refresh token");
         }
+    }
+
+    private String delegateAccessToken(Member member) { // 코드의 중복...찝찝하다.
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberEmail", member.getEmail());
+        claims.put("roles", member.getRoles());
+        claims.put("memberNickName", member.getMemberNickName());
+
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
     }
 } // 나중에 리플래쉬 토큰도 같이 재발급해줄까?..상의해보자
